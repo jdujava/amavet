edcbe45bc4a9a3678b0ed2c010057a57262d2d2c
strakam <matejstraka@icloud.com>
2017-01-19 22:54:16 +0100

    ok dnes nato serem, je tu zakladny JS template, viac dorobim zajtra alebo nwm kedy.. kto chce moze vyriesit to, ze ked prichadza text tak aby prichadzal z oapcity 0 na 1 alebo tak.. v dalsom update zakomentujem cssko aj js teraz idem spat

--- web/main/jscript.js
+++ web/main/jscript.js
@@ -1,66 +1,33 @@
 $(document).ready(function(){
-	var current_page = $('title').text();
-	var item_offset;
-	var offset;
-	var element_pos;
-	var i = 0;
-	var t = 1;
-	var margin_left = 0;
-	var doc_height = $(document).height();
-	$('#knife').addClass('element-animation');
-	$('#fork').addClass('element-animation');
-	$('.horizontal').animate({'width': '450px'}, 1500);
-	$('.slider_text p').css('opacity', '0');
-	$('.slider_text p:eq(4)').css('opacity', '1');
-	var sliding = setInterval(slider,6000);
-	function slider(){
-		if(i == 4)
-		{
-			$('.leader').css('margin-left', '0px');
-			margin_left = -1000;
-			$('.leader').animate({'margin-left': '' + margin_left + 'px'},2000);
-			$('.slider_text p').animate({"opacity": "0"}, 300);
-			$('.slider_text p:eq(1)').delay(500).animate({"opacity": "1"}, 2500);
-			i=1;
-			t=2;
-		}
-		else
-		{
-			margin_left -= 1000;
-			$('.slider_text p').animate({"opacity": "0"}, 300);
-			$('.slider_text p:eq(' + t + ')').delay(500).animate({"opacity": "1"}, 2500);
-			$('.leader').animate({'margin-left': '' + margin_left + 'px'},2000);
-			t +=1;
-			i +=1;
-		}
-	}
+  var win_offset;
+  var this_offset;
+  var win_height = $(window).height();
+  var i = 1;
+  setTimeout(function() {
+    $('.page_1 .scroll_trigger').addClass('smooth');
+  },1000);
+  $('.page_1 .right_sheet').animate({height: '80px'}, 1000);
+  $('.page_1 h1').addClass('comin_in_hot');
+  $('.page_1 p').addClass('comin_in_hot');
+  $('nav a').hover(function() {
+		var ppos = $(this).offset();
+		$('.a_border').css("left",ppos.left);
+  	$('.a_border').toggleClass('nav_hover');
+  }, function() {
+		$('.a_border').toggleClass('nav_hover');
+  });
+
+  $(window).on('scroll', function() {
+      this_offset = $('.scroll_trigger').eq(i).offset().top;
+      win_offset = $(window).scrollTop() + win_height;
+      if(win_offset-250>this_offset){
+        setTimeout(function(integer) {
+          $('.scroll_trigger').eq(integer).addClass('smooth');
+        }.bind(null, i), 1000);
+        $('.right_sheet').eq(i).animate({height: '80px'}, 1000);
+        $('.page_'+ (i+1)+ ' h1').addClass('comin_in_hot');
+        $('.page_'+ (i+1)+ ' p').addClass('comin_in_hot');
+        i++;
+      }
+  });
 });
-
-/*$(window).on("scroll", function(){
-		item_offset = $(window).scrollTop()-116;
-		offset = $(window).scrollTop();
-		element_pos = $('footer h3').offset().top;
-		console.log(offset);
-		if(item_offset > 0 && item_offset<817)
-		{
-			$('.menu1').css("background-size", "" + (120 - (item_offset/40.85)) + "%", + (120 - (item_offset/40.85)) + "%");
-			$('.menu2').css("background-size", "" + (120 - (item_offset/40.85)) + "%", + (120 - (item_offset/40.85)) + "%");
-		}
-		if (offset + $(window).height() >= element_pos)
-		{
-			$('.hnedypasik').animate({"width": "180px"}, 700);
-			var delay = 300;
-			var progress = setInterval(cycle, delay);
-
-			var index = 1;
-			function cycle(){
-				var dis = document.getElementById('' + index);
-				$(dis).addClass('fade_in');
-				index ++;
-
-				if(index>5){
-					clearInterval(progress);
-				}
-			};
-		}
-	});*/
